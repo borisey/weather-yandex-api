094f5adce27488c530000d3fe99b2ffcca70c84d
borisey <slamsnv@gmail.com>
2024-10-14 02:38:41 +0300

    Show temperature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import javax.json.*;
+import java.io.StringReader;
+
 public class Main {
     public static void main(String[] args) {
         Weather weather = new Weather();
@@ -7,15 +10,14 @@ public class Main {
 
         System.out.println(weatherJson);
 
-//            JsonReader reader = Json.createReader(new StringReader(jsonString));
-//
-//            JsonStructure jsonStructure = reader.read();
-//
-//            if (jsonStructure.getValueType() == JsonValue.ValueType.OBJECT) {
-//                JsonObject jsonObject = (JsonObject) jsonStructure;
-//                int fact = jsonObject.getInt("now");
-//                System.out.println("Temperature: " + fact);
-//            }
+        JsonReader reader = Json.createReader(new StringReader(weatherJson));
 
+        JsonStructure jsonStructure = reader.read();
+
+        if (jsonStructure.getValueType() == JsonValue.ValueType.OBJECT) {
+            JsonObject jsonObject = (JsonObject) jsonStructure;
+            int fact = jsonObject.getInt("now");
+            System.out.println("Temperature: " + fact);
+        }
     }
 }
