b95a95787b6c59375aac2bcfb986a4c6636f6c7b
borisey <slamsnv@gmail.com>
2024-10-14 18:55:19 +0300

    average temperature for a certain period

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,11 @@ import org.example.services.WeatherService;
 public class Main {
     public static void main(String[] args) {
         WeatherService weatherService = new WeatherService();
+        GsonParser parser             = new GsonParser();
+        String allWeatherData         = weatherService.getWeather();
+        Root root                     = parser.parse(allWeatherData);
 
-        GsonParser parser = new GsonParser();
-        Root root = parser.parse(weatherService.getWeather());
-
-        System.out.println("All weather data: " + weatherService.getWeather());
+        System.out.println("All weather data: " + allWeatherData);
         System.out.println("Current temperature: " + weatherService.getCurrentTemperature(root));
         System.out.println("Average temperature for next " + WeatherService.DAYS_LIMIT + " days: " + weatherService.getAverageTemperature(root));
     }
