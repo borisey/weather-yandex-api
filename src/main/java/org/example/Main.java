343e30b086a02cc9b6c5436f8ac2e80ea0129cbe
borisey <slamsnv@gmail.com>
2024-10-14 12:45:24 +0300

    average temperature for a certain period

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int limit = 1;
+        int limit = 5;
 
         Weather weather = new Weather();
         String weatherJson = weather.getWeather(limit);
