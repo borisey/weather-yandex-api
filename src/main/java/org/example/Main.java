df5aba49c33accbc05640f8bd034f541fa1fcdcf
borisey <slamsnv@gmail.com>
2024-10-14 18:04:42 +0300

    average temperature for a certain period

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int weeksLimit = 10;
+        int weeksLimit = 5;
 
         WeatherRequest weatherRequest = new WeatherRequest();
         String weatherJson = weatherRequest.getWeather(weeksLimit);
@@ -23,12 +23,22 @@ public class Main {
 
         List<Forecasts> forecasts = root.getForecasts();
 
+        int allTemps = 0;
+        float averageTemperature = 0F;
+
         for (var i = 0; i < forecasts.size(); i++) {
             Forecasts forecast = forecasts.get(i);
             Parts parts = forecast.getParts();
 
             System.out.println(parts.getDay().getTempAvg());
+
+            allTemps += parts.getDay().getTempAvg();
+            averageTemperature = (float) allTemps / weeksLimit;
         }
+
+        System.out.println(allTemps);
+
+        System.out.println(averageTemperature);
 
 //        System.out.println("Average temperature for " + weeksLimit + " weeks: " + root.getForecasts());
     }
