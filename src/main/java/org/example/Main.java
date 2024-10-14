eaf9b6aa7c6e61103a0703597d765a4348304900
borisey <slamsnv@gmail.com>
2024-10-14 18:35:41 +0300

    average temperature for a certain period

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,18 @@
 package org.example;
 
-import org.example.model.Forecasts;
-import org.example.model.Parts;
 import org.example.model.Root;
-
-import java.util.List;
 
 public class Main {
     static final int DAYS_LIMIT = 5;
 
     public static void main(String[] args) {
-        GsonParser parser = new GsonParser();
-        Root root = parser.parse(getAllData());
-
-        System.out.println("All data: " + getAllData());
-        System.out.println("Current temperature: " + getCurrentTemperature(root));
-        System.out.println("Average temperature for next " + DAYS_LIMIT + " days: " + getAverageTemperature(root));
-    }
-
-    private static String getAllData() {
         WeatherRequest weatherRequest = new WeatherRequest();
 
-        return weatherRequest.getWeather(DAYS_LIMIT);
-    }
+        GsonParser parser = new GsonParser();
+        Root root = parser.parse(weatherRequest.getAllData());
 
-    private static int getCurrentTemperature(Root root) {
-        return root.getFact().getTemp();
-    }
-
-    private static float getAverageTemperature(Root root) {
-        List<Forecasts> forecasts = root.getForecasts();
-        int allTemperatures = 0;
-        float averageTemperature = 0F;
-
-        for (Forecasts forecast : forecasts) {
-            Parts parts = forecast.getParts();
-            allTemperatures += parts.getDay().getTempAvg();
-            averageTemperature = (float) allTemperatures / DAYS_LIMIT;
-        }
-
-        return averageTemperature;
+        System.out.println("All data: " + weatherRequest.getAllData());
+        System.out.println("Current temperature: " + weatherRequest.getCurrentTemperature(root));
+        System.out.println("Average temperature for next " + DAYS_LIMIT + " days: " + weatherRequest.getAverageTemperature(root));
     }
 }
--- src/main/java/org/example/WeatherRequest.java
+++ src/main/java/org/example/WeatherRequest.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import org.example.model.Forecasts;
+import org.example.model.Parts;
+import org.example.model.Root;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class WeatherRequest {
     static final Float LAT      = 55.75F;
     static final Float LON      = 37.62F;
+    static final int DAYS_LIMIT = 5;
     static final String URL     = "https://api.weather.yandex.ru/v2/forecast?lat=" + LAT + "&lon=" + LON + "&limit=";
     static final String API_KEY = "6c9d00db-7689-4b56-bbf1-026e2c0c12e3";
 
@@ -28,5 +34,29 @@ public class WeatherRequest {
         }
 
         return response.body();
+    }
+
+    public String getAllData() {
+        WeatherRequest weatherRequest = new WeatherRequest();
+
+        return weatherRequest.getWeather(DAYS_LIMIT);
+    }
+
+    public int getCurrentTemperature(Root root) {
+        return root.getFact().getTemp();
+    }
+
+    public float getAverageTemperature(Root root) {
+        List<Forecasts> forecasts = root.getForecasts();
+        int allTemperatures = 0;
+        float averageTemperature = 0F;
+
+        for (Forecasts forecast : forecasts) {
+            Parts parts = forecast.getParts();
+            allTemperatures += parts.getDay().getTempAvg();
+            averageTemperature = (float) allTemperatures / DAYS_LIMIT;
+        }
+
+        return averageTemperature;
     }
 }
