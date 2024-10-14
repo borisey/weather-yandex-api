8b2e58b61b8ca82ef3f2be2e460e210ae634530c
borisey <slamsnv@gmail.com>
2024-10-14 18:40:16 +0300

    average temperature for a certain period

--- src/main/java/org/example/WeatherRequest.java
+++ src/main/java/org/example/WeatherRequest.java
@@ -13,13 +13,13 @@ public class WeatherRequest {
     static final Float LAT      = 55.75F;
     static final Float LON      = 37.62F;
     static final int DAYS_LIMIT = 5;
-    static final String URL     = "https://api.weather.yandex.ru/v2/forecast?lat=" + LAT + "&lon=" + LON + "&limit=";
+    static final String URL     = "https://api.weather.yandex.ru/v2/forecast?lat=" + LAT + "&lon=" + LON + "&limit=" + DAYS_LIMIT;
     static final String API_KEY = "6c9d00db-7689-4b56-bbf1-026e2c0c12e3";
 
-    public String getWeather(int weeksLimit) {
+    public String getWeather() {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URL + weeksLimit))
+                .uri(URI.create(URL))
                 .header("X-Yandex-Weather-Key", API_KEY)
                 .GET()
                 .build();
@@ -38,7 +38,7 @@ public class WeatherRequest {
     public String getAllData() {
         WeatherRequest weatherRequest = new WeatherRequest();
 
-        return weatherRequest.getWeather(DAYS_LIMIT);
+        return weatherRequest.getWeather();
     }
 
     public int getCurrentTemperature(Root root) {
